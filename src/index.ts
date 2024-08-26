379f2987946e6bb75135f4e7543a8521f63cb166
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-26 22:58:19 +0700

    -- cache

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-client.once('ready', () => {
-  console.log('Bot is online!');
-});
-
-client.login('YOUR_BOT_TOKEN'); // Replace with your bot token
